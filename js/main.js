a87268f37e33becdc977419a28226c3e00e292e4
Susanto <nerdyspook007@gmail.com>
2021-08-15 17:49:17 +0530

    feat: minion translation api added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-    item.addEventListener("click", function () {
-        this.classList.toggle("collapsible--expanded")
-    })
-);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text) {
+    return serverUrl + "?text=" + text
+}
+
+function clickHandler() {
+    
+    var input = txtInput.value;
+
+    fetch(getTranslationUrl(input))
+        .then(response => response.json())
+        .then(json => {
+            outputDiv.innerText = json.contents.translated;
+            console.log(json.contents.translated)
+        })
+}
+
+btnTranslate.addEventListener("click", clickHandler)
